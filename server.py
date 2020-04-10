2b8d194fa7fe0bc1a9a8e8475915be6467b6da81
Patrick D <patrick.doody@rgi-corp.com>
2020-04-09 20:26:14 -0400

    Color Endpoint

--- server.py
+++ server.py
@@ -7,12 +7,24 @@ import colorsys
 from flask import Flask
 from dotstar import Adafruit_DotStar
 
+# Startup the LED
+def init_led(numPixels):
+	strip = Adafruit_DotStar(numPixels, 12000000)
+	strip.begin()
+	strip.setBrightness(32)
+	return strip
+
+led_count = 72
+strip = init_led(led_count)
+
 app = Flask(__name__)
 
 @app.route('/')
 def healthCheck():
-	return 'Healthy.'
+	return 'Up.'
 
-@app.route('/color/<color>', methods=['PUT'])
+@app.route('/color/<string:color>', methods=['PUT'])
 def changeColor(color):
+	for x in range(led_count):
+		strip.setPixelColor(x, int(color), 16)
 	return flask.Response(status=200)
